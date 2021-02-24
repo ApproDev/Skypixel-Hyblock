5fcd054db352beda37cbdb63d8398232f99e205e
Josh123likeme <79576357+Josh123likeme@users.noreply.github.com>
2021-02-24 11:15:50 +0000

    Add files via upload

---
+++ Skypixel Hyblock/src/com/BadDevelopers/SkypixelHyblock/Currency/Currency.java
@@ -0,0 +1,10 @@
+package com.BadDevelopers.SkypixelHyblock.Currency;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class Currency {
+	
+	public HashMap<UUID, Integer> moneyGain = new HashMap<UUID, Integer>();
+	
+}
