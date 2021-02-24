407fdcd0ac07a23a01a5d02ef3993312a4987841
Josh123likeme <79576357+Josh123likeme@users.noreply.github.com>
2021-02-24 20:43:45 +0000

    Delete src directory

--- src/com/BadDevelopers/SkypixelHyblock/Command.java
+++
@@ -1,9 +0,0 @@
-package com.BadDevelopers.SkypixelHyblock;
-
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.TabCompleter;
-
-public abstract class Command implements CommandExecutor {
-	public TabCompleter completer;
-	public String name;
-}
